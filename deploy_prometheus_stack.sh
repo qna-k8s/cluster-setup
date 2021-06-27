3bf31211c9d459c80e1cda973395b178731412ba
Trivedh Audurthi <audurthi.t@northeastern.edu>
2021-06-27 15:27:31 -0400

    enabled kubeDns

--- deploy_prometheus_stack.sh
+++ deploy_prometheus_stack.sh
@@ -2,9 +2,9 @@
 var=$(helm status monitor-stack 2>&1)
 errorMsg="Error: release: not found"
 installCmd="helm install monitor-stack ./kube-prometheus-stack \
-            --set grafana.service.type=NodePort,grafana.service.nodePort=30020,kubeDns.enabled=true"
+            -f metric_config.yaml"
 updateCmd="helm upgrade monitor-stack ./kube-prometheus-stack \
-           --set grafana.service.type=NodePort,grafana.service.nodePort=30020,kubeDns.enabled=true"
+           -f metric_config.yaml"
 if [ "$var" = "$errorMsg" ]
 then
     eval $installCmd
