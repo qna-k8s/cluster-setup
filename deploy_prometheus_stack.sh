d7ca0e2a85ac2f9c11636a9a38da66a5849dcda4
Trivedh Audurthi <audurthi.t@northeastern.edu>
2021-06-27 12:00:59 -0400

    fixing promethues deployment

--- deploy_prometheus_stack.sh
+++ deploy_prometheus_stack.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 var=$(helm status monitor-stack 2>&1)
 errorMsg="Error: release: not found"
-installCmd="helm install monitor-stack ./kube-promatheus-stack \
+installCmd="helm install monitor-stack ./kube-prometheus-stack \
             --set grafana.service.type=NodePort,grafana.service.nodePort=30020"
-updateCmd="helm upgrade monitor-stack ./kube-promatheus-stack \
+updateCmd="helm upgrade monitor-stack ./kube-prometheus-stack \
            --set grafana.service.type=NodePort,grafana.service.nodePort=30020"
 if [ "$var" = "$errorMsg" ]
 then
